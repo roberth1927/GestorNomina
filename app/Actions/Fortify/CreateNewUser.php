e9df2676e504348ea1fb70f7bb15a3a0c9348f7b
Roberth <robinmoralesquiroz@gmail.com>
2024-07-05 23:31:20 -0500

    client role is assigned to new users

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use Spatie\Permission\Models\Role;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -27,10 +28,18 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        // Asignar el rol "client" al nuevo usuario
+        $clientRole = Role::where('name', 'client')->first();
+        if ($clientRole) {
+            $user->assignRole($clientRole);
+        }
+
+        return $user;
     }
 }
